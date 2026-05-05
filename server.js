70b677262481610c61b3f48fbdf2a169faec16e0
oshidakaori-lab <oshidakaori@gmail.com>
2026-05-05 14:24:49 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -22,6 +22,26 @@ app.post("/callback", line.middleware(config), async (req, res) => {
 
 if (event.type === "message") {
 
+const skyImages = {
+  "晴れ": "https://i.imgur.com/gKnEQds.jpeg",
+  "曇り": "https://i.imgur.com/PNvbK3W.jpeg",
+  "雨": "https://i.imgur.com/WC8C8zC.jpeg",
+  "風": "https://i.imgur.com/9kFUKDI.jpeg",
+  "雷": "https://i.imgur.com/etZ12NJ.jpeg"
+};
+  
+await client.replyMessage(event.replyToken, [
+  {
+    type: "image",
+    originalContentUrl: imageUrl,
+    previewImageUrl: imageUrl
+  },
+  {
+    type: "text",
+    text: `今日の空：${weather}`
+  }
+]);  
+  
   const imageUrl = "https://i.imgur.com/gKnEQds.jpeg";
 
   await client.replyMessage(event.replyToken, [
